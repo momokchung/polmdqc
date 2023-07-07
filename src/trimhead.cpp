918c4f8fd3ba8518d26322e8ab750518402acd2d
Moses Chung <kchung25@wustl.edu>
2023-07-07 17:47:32 -0500

    trimhead implementation

--- src/trimhead.cpp
+++ src/trimhead.cpp
@@ -1,4 +1,20 @@
+/////////////////////////////////////////////////////////
+//                                                     //
+//  trimhead.cpp  --  remove spaces before first text  //
+//                                                     //
+/////////////////////////////////////////////////////////
 
-// {
+// "trimhead" removes blank spaces before the first non-blank
+// character in a text string by shifting the string to the left
 
-// }
+
+#include "trimhead.h"
+
+void trimhead(std::string& string)
+{
+    size_t firstNonBlank = string.find_first_not_of(' ');
+
+    if (firstNonBlank != std::string::npos && firstNonBlank > 0) {
+        string = string.substr(firstNonBlank);
+    }
+}
