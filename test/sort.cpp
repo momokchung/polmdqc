91ad252406ad15ec412bbab45cb8d84b5af80930
Moses Chung <kchung25@wustl.edu>
2023-07-29 00:48:01 +0900

    clean getnumb, add double to sort test

--- test/sort.cpp
+++ test/sort.cpp
@@ -4,6 +4,10 @@
 TEST_CASE("sort") {
     std::vector<int> numbers = {5, 2, 8, 2, 1, 5, 9, 8, 2};
     std::vector<int> sortedNumbers = {1, 2, 5, 8, 9};
-    sort<int>(numbers);
+    std::vector<double> doubles = {1.1, 2.2, -3.3, -2.2, 2.2, 3.3, -1.1, 0., 2.2, -3.3};
+    std::vector<double> sortedDoubles = {-3.3, -2.2, -1.1, 0., 1.1, 2.2, 3.3};
+    sort(numbers);
     REQUIRE(numbers == sortedNumbers);
+    sort(doubles);
+    REQUIRE(doubles == sortedDoubles);
 }
