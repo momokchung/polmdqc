fec6b6a8cae4dd498ea01caf916f40754285410a
Moses Chung <kchung25@wustl.edu>
2023-08-16 15:05:35 -0700

    empole analysis implementation

---
+++ include/damping.h
@@ -0,0 +1,10 @@
+////////////////////////////////////////////////
+//                                            //
+//  damping.h  --  various damping functions  //
+//                                            //
+////////////////////////////////////////////////
+
+
+#pragma once
+
+void damppole(double r,int rorder, double alphai, double alphak, double* dmpi, double* dmpk, double* dmpik);
--- src/empole.cpp
+++ src/empole.cpp
@@ -60,6 +60,7 @@ void empole(calcMode calculationMode)
 #include "chkpole.h"
 #include "couple.h"
 #include "cutoffSwitch.h"
+#include "damping.h"
 #include "energi.h"
 #include "group.h"
 #include "groups.h"
@@ -74,6 +75,7 @@ void empole(calcMode calculationMode)
 #include "rotpole.h"
 #include "shunt.h"
 #include "usage.h"
+#include <cmath>
 
 void empole_a(calcMode calculationMode)
 {
@@ -210,145 +212,128 @@ void empole_a(calcMode calculationMode)
                 zr = z[k] - zi;
                 if (use_bounds) image(xr,yr,zr);
                 r2 = xr*xr + yr* yr + zr*zr;
-//                if (r2 .le. off2) then
-//                   r = sqrt(r2)
-//                   ck = rpole(1,k)
-//                   dkx = rpole(2,k)
-//                   dky = rpole(3,k)
-//                   dkz = rpole(4,k)
-//                   qkxx = rpole(5,k)
-//                   qkxy = rpole(6,k)
-//                   qkxz = rpole(7,k)
-//                   qkyy = rpole(9,k)
-//                   qkyz = rpole(10,k)
-//                   qkzz = rpole(13,k)
-// c
-// c     intermediates involving moments and separation distance
-// c
-//                   dir = dix*xr + diy*yr + diz*zr
-//                   qix = qixx*xr + qixy*yr + qixz*zr
-//                   qiy = qixy*xr + qiyy*yr + qiyz*zr
-//                   qiz = qixz*xr + qiyz*yr + qizz*zr
-//                   qir = qix*xr + qiy*yr + qiz*zr
-//                   dkr = dkx*xr + dky*yr + dkz*zr
-//                   qkx = qkxx*xr + qkxy*yr + qkxz*zr
-//                   qky = qkxy*xr + qkyy*yr + qkyz*zr
-//                   qkz = qkxz*xr + qkyz*yr + qkzz*zr
-//                   qkr = qkx*xr + qky*yr + qkz*zr
-//                   dik = dix*dkx + diy*dky + diz*dkz
-//                   qik = qix*qkx + qiy*qky + qiz*qkz
-//                   diqk = dix*qkx + diy*qky + diz*qkz
-//                   dkqi = dkx*qix + dky*qiy + dkz*qiz
-//                   qiqk = 2.*(qixy*qkxy+qixz*qkxz+qiyz*qkyz)
-//      &                      + qixx*qkxx + qiyy*qkyy + qizz*qkzz
-// c
-// c     get reciprocal distance terms for this interaction
-// c
-//                   rr1 = f * mscale(k) / r
-//                   rr3 = rr1 / r2
-//                   rr5 = 3. * rr3 / r2
-//                   rr7 = 5. * rr5 / r2
-//                   rr9 = 7. * rr7 / r2
-// c
-// c     find damped multipole intermediates and energy value
-// c
-//                   if (use_chgpen) then
-//                      corek = pcore(k)
-//                      valk = pval(k)
-//                      alphak = palpha(k)
-//                      term1 = corei*corek
-//                      term1i = corek*vali
-//                      term2i = corek*dir
-//                      term3i = corek*qir
-//                      term1k = corei*valk
-//                      term2k = -corei*dkr
-//                      term3k = corei*qkr
-//                      term1ik = vali*valk
-//                      term2ik = valk*dir - vali*dkr + dik
-//                      term3ik = vali*qkr + valk*qir - dir*dkr
-//      &                            + 2.*(dkqi-diqk+qiqk)
-//                      term4ik = dir*qkr - dkr*qir - 4.*qik
-//                      term5ik = qir*qkr
-//                      call damppole (r,9,alphai,alphak,
-//      &                               dmpi,dmpk,dmpik)
-//                      rr1i = dmpi(1)*rr1
-//                      rr3i = dmpi(3)*rr3
-//                      rr5i = dmpi(5)*rr5
-//                      rr1k = dmpk(1)*rr1
-//                      rr3k = dmpk(3)*rr3
-//                      rr5k = dmpk(5)*rr5
-//                      rr1ik = dmpik(1)*rr1
-//                      rr3ik = dmpik(3)*rr3
-//                      rr5ik = dmpik(5)*rr5
-//                      rr7ik = dmpik(7)*rr7
-//                      rr9ik = dmpik(9)*rr9
-//                      e = term1*rr1 + term1i*rr1i
-//      &                      + term1k*rr1k + term1ik*rr1ik
-//      &                      + term2i*rr3i + term2k*rr3k
-//      &                      + term2ik*rr3ik + term3i*rr5i
-//      &                      + term3k*rr5k + term3ik*rr5ik
-//      &                      + term4ik*rr7ik + term5ik*rr9ik
-// c
-// c     find standard multipole intermediates and energy value
-// c
-//                   else
-//                      term1 = ci*ck
-//                      term2 = ck*dir - ci*dkr + dik
-//                      term3 = ci*qkr + ck*qir - dir*dkr
-//      &                          + 2.*(dkqi-diqk+qiqk)
-//                      term4 = dir*qkr - dkr*qir - 4.*qik
-//                      term5 = qir*qkr
-//                      e = term1*rr1 + term2*rr3 + term3*rr5
-//      &                      + term4*rr7 + term5*rr9
-//                   end if
-// c
-// c     increment the overall multipole energy components
-// c
-//                   if (use_group)  e = e * fgrp
-//                   if (e .ne. 0.) then
-//                      nem = nem + 1
-//                      em = em + e
-//                      aem(i) = aem(i) + 0.5*e
-//                      aem(k) = aem(k) + 0.5*e
-//                      if (molcule(i) .ne. molcule(k)) then
-//                         einter = einter + e
-//                      end if
-//                   end if
-// c
-// c     print message if the energy of this interaction is large
-// c
-//                   huge = (abs(e) .gt. 100.)
-//                   if ((debug.and.e.ne.0.)
-//      &                  .or. (verbose.and.huge)) then
-//                      if (header) then
-//                         header = .false.
-//                         write (iout,20)
-//    20                   format (/,' Individual Atomic Multipole',
-//      &                             ' Interactions :',
-//      &                          //,' Type',14x,'Atom Names',
-//      &                             15x,'Distance',8x,'Energy',/)
-//                      end if
-//                      write (iout,30)  i,name(i),k,name(k),r,e
-//    30                format (' Mpole',5x,2(i7,'-',a3),9x,
-//      &                          f10.4,2x,f12.4)
-//                   end if
-//                end if
+                if (r2 <= off2) {
+                    r = std::sqrt(r2);
+                    ck = rpole[k][0];
+                    dkx = rpole[k][1];
+                    dky = rpole[k][2];
+                    dkz = rpole[k][3];
+                    qkxx = rpole[k][4];
+                    qkxy = rpole[k][5];
+                    qkxz = rpole[k][6];
+                    qkyy = rpole[k][8];
+                    qkyz = rpole[k][9];
+                    qkzz = rpole[k][12];
+
+                    // intermediates involving moments and separation distance
+                    dir = dix*xr + diy*yr + diz*zr;
+                    qix = qixx*xr + qixy*yr + qixz*zr;
+                    qiy = qixy*xr + qiyy*yr + qiyz*zr;
+                    qiz = qixz*xr + qiyz*yr + qizz*zr;
+                    qir = qix*xr + qiy*yr + qiz*zr;
+                    dkr = dkx*xr + dky*yr + dkz*zr;
+                    qkx = qkxx*xr + qkxy*yr + qkxz*zr;
+                    qky = qkxy*xr + qkyy*yr + qkyz*zr;
+                    qkz = qkxz*xr + qkyz*yr + qkzz*zr;
+                    qkr = qkx*xr + qky*yr + qkz*zr;
+                    dik = dix*dkx + diy*dky + diz*dkz;
+                    qik = qix*qkx + qiy*qky + qiz*qkz;
+                    diqk = dix*qkx + diy*qky + diz*qkz;
+                    dkqi = dkx*qix + dky*qiy + dkz*qiz;
+                    qiqk = 2.*(qixy*qkxy+qixz*qkxz+qiyz*qkyz) + qixx*qkxx + qiyy*qkyy + qizz*qkzz;
+
+                    // get reciprocal distance terms for this interaction
+                    rr1 = f * mscale[k] / r;
+                    rr3 = rr1 / r2;
+                    rr5 = 3. * rr3 / r2;
+                    rr7 = 5. * rr5 / r2;
+                    rr9 = 7. * rr7 / r2;
+
+                    // find damped multipole intermediates and energy value
+                    if (use_chgpen) {
+                        corek = pcore[k];
+                        valk = pval[k];
+                        alphak = palpha[k];
+                        term1 = corei*corek;
+                        term1i = corek*vali;
+                        term2i = corek*dir;
+                        term3i = corek*qir;
+                        term1k = corei*valk;
+                        term2k = -corei*dkr;
+                        term3k = corei*qkr;
+                        term1ik = vali*valk;
+                        term2ik = valk*dir - vali*dkr + dik;
+                        term3ik = vali*qkr + valk*qir - dir*dkr + 2.*(dkqi-diqk+qiqk);
+                        term4ik = dir*qkr - dkr*qir - 4.*qik;
+                        term5ik = qir*qkr;
+                        damppole(r,9,alphai,alphak,dmpi,dmpk,dmpik);
+                        rr1i = dmpi[0]*rr1;
+                        rr3i = dmpi[2]*rr3;
+                        rr5i = dmpi[4]*rr5;
+                        rr1k = dmpk[0]*rr1;
+                        rr3k = dmpk[2]*rr3;
+                        rr5k = dmpk[4]*rr5;
+                        rr1ik = dmpik[0]*rr1;
+                        rr3ik = dmpik[2]*rr3;
+                        rr5ik = dmpik[4]*rr5;
+                        rr7ik = dmpik[6]*rr7;
+                        rr9ik = dmpik[8]*rr9;
+                        e = term1*rr1 + term1i*rr1i
+                          + term1k*rr1k + term1ik*rr1ik
+                          + term2i*rr3i + term2k*rr3k
+                          + term2ik*rr3ik + term3i*rr5i
+                          + term3k*rr5k + term3ik*rr5ik
+                          + term4ik*rr7ik + term5ik*rr9ik;
+                    }
+
+                    // find standard multipole intermediates and energy value
+                    else {
+                        term1 = ci*ck;
+                        term2 = ck*dir - ci*dkr + dik;
+                        term3 = ci*qkr + ck*qir - dir*dkr + 2.*(dkqi-diqk+qiqk);
+                        term4 = dir*qkr - dkr*qir - 4.*qik;
+                        term5 = qir*qkr;
+                        e = term1*rr1 + term2*rr3 + term3*rr5 + term4*rr7 + term5*rr9;
+                    }
+
+                    // increment the overall multipole energy components
+                    if (use_group) e = e * fgrp;
+                    if (e != 0.) {
+                        nem++;
+                        em += e;
+                        aem[i] = aem[i] + 0.5*e;
+                        aem[k] = aem[k] + 0.5*e;
+                        if (molcule[i] != molcule[k]) {
+                            einter += e;
+                        }
+                    }
+
+                    // print message if the energy of this interaction is large
+                    huge = (std::abs(e) > 100.);
+                    if ((debug and e!=0.) or (verbose and huge)) {
+                        if (header) {
+                            header = false;
+                            printf("\n Individual Atomic Multipole Interactions :\n\n");
+                            printf(" Type              Atom Names               Distance        Energy\n\n");
+                        }
+                        printf(" Mpole     %7d-%-3s%7d-%-3s         %10.4f  %12.4f\n", i+1,name[i].c_str(),k+1,name[k].c_str(),r,e);
+                    }
+                }
             }
         }
-// c
-// c     reset exclusion coefficients for connected atoms
-// c
-//          do j = 1, n12(i)
-//             mscale(i12(j,i)) = 1.
-//          end do
-//          do j = 1, n13(i)
-//             mscale(i13(j,i)) = 1.
-//          end do
-//          do j = 1, n14(i)
-//             mscale(i14(j,i)) = 1.
-//          end do
-//          do j = 1, n15(i)
-//             mscale(i15(j,i)) = 1.
+        // reset exclusion coefficients for connected atoms
+        for (int j = 0; j < n12[i]; j++) {
+            mscale[i12[i][j]] = 1.;
+        }
+        for (int j = 0; j < n13[i]; j++) {
+            mscale[i13[i][j]] = 1.;
+        }
+        for (int j = 0; j < n14[i]; j++) {
+            mscale[i14[i][j]] = 1.;
+        }
+        for (int j = 0; j < n15[i]; j++) {
+            mscale[i15[i][j]] = 1.;
+        }
     }
 }
 
