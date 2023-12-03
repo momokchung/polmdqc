d42ab0d0fee56f8e80a788e56ce0df10cb1f3a6f
momokchung <kchung25@wustl.edu>
2023-12-03 17:51:14 -0600

    libint2 imported

--- src/hartree.cpp
+++ src/hartree.cpp
@@ -57,7 +57,6 @@ std::vector<real> wS;
 // need to implement other convergence methods as well (damping, 2nd order, etc.)
 void rhf()
 {
-    // libint2::initialize();
     int N = basis::N;
     int N2 = N * N;
     int nElec = atoms::nElec;
@@ -117,7 +116,49 @@ void rhf()
     guess();
 
     // conventional scf
-    // libint2::finalize();
+
+    libint2::initialize();
+    std::string xyzfilename = "/home/kchung25/polqcmd/example/QMexample/tmp.xyz";
+    std::ifstream input_file(xyzfilename);
+    std::vector<libint2::Atom> atoms = libint2::read_dotxyz(input_file);
+    libint2::BasisSet obs("3-21g", atoms);
+
+    libint2::Engine s_engine(libint2::Operator::overlap,  // will compute overlap ints
+                            obs.max_nprim(),              // max # of primitives in shells this engine will accept
+                            obs.max_l()                   // max angular momentum of shells this engine will accept
+                            );
+    //
+    auto shell2bf = obs.shell2bf(); // maps shell index to basis function index
+                                    // shell2bf[0] = index of the first basis function in shell 0
+                                    // shell2bf[1] = index of the first basis function in shell 1
+                                    // ...
+    const auto& buf_vec = s_engine.results(); // will point to computed shell sets
+                                              // const auto& is very important!
+
+    for(auto s1=0; s1!=obs.size(); ++s1) {
+        for(auto s2=0; s2!=obs.size(); ++s2) {
+
+            std::cout << "compute shell set {" << s1 << "," << s2 << "} ... ";
+            s_engine.compute(obs[s1], obs[s2]);
+            std::cout << "done" << std::endl;
+            auto ints_shellset = buf_vec[0];  // location of the computed integrals
+            if (ints_shellset == nullptr)
+                continue;  // nullptr returned if the entire shell-set was screened out
+
+            auto bf1 = shell2bf[s1];  // first basis function in first shell
+            auto n1 = obs[s1].size(); // number of basis functions in first shell
+            auto bf2 = shell2bf[s2];  // first basis function in second shell
+            auto n2 = obs[s2].size(); // number of basis functions in second shell
+
+            // integrals are packed into ints_shellset in row-major (C) form
+            // this iterates over integrals in this order
+            for(auto f1=0; f1!=n1; ++f1)
+                for(auto f2=0; f2!=n2; ++f2)
+                    std::cout << "  " << bf1+f1 << " " << bf2+f2 << " " << ints_shellset[f1*n2+f2] << std::endl;
+        }
+    }
+    
+    libint2::finalize();
 }
 
 
